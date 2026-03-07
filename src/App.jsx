35ae0cd49e8b69a5a51ac7b34d9be86f4170eca5
unknown <garvitkul@gmail.com>
2026-03-07 19:35:50 +0530

    Add React Pokedex Project

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import { useState,useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PokeBall from './resources/pokeball.svg'
+import Search from './resources/search.svg'
+import Poke from './Poke.jsx';
+
+export const capitalize = (str = "") => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+function App() {
+  const [count, setCount] = useState(0);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [input, setInput] = useState('');
+  const [pokemon, setPokemon] = useState(null);
+  const [loading, setLoading] = useState(false);
+  
+  const searchPoke = async (name) => {
+    try{
+      if (!name.trim()) return;
+
+      setLoading(true);
+      setPokemon(null);
+
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+      if (!response.ok) {
+        throw new Error("Pokemon not found!");
+      }
+
+
+      const data = await response.json();
+      setPokemon(data);
+    }
+    catch (err) {
+      console.log(err.message);
+      setPokemon(null);
+    }
+    finally {
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    searchPoke(searchTerm);
+  },[searchTerm]);
+  useEffect(() => {
+  console.log(pokemon);
+  },[pokemon]);
+
+  return (
+    <div className="min-h-screen w-screen flex flex-col">
+      <div className="flex items-center justify-start bg-[#ffdcdc] h-[15vh] border-8 border-black p-5 w-full">
+        <img
+         src={PokeBall}
+         width={80}
+         className="md:w-25"
+        />
+        <p className="font-bold text-black flex text-6xl pixel p-8  mr-[35%]"> POKEDEX </p>
+        
+        <input value={input} onChange={(e) => setInput(e.target.value)} type="text" className="pixel  text-black border-black border-3 h-10 w-[20rem] placeholder:text-gray-800 pl-4 placeholder:pixel" placeholder="ENTER A POKEMON"/>
+        <img
+         className="md:pl-3"
+         src={Search}
+         width={53}
+         onClick={() => setSearchTerm(input)}
+        />
+      </div>
+      {loading? (
+        <div className='flex items-center justify-center h-[85vh]'>
+        <h1 className='text-amber-950 text-center pixel'> Please Wait </h1>
+      </div>
+      ): pokemon? (
+        <div className='flex  min-h-[85vh]'>
+        <Poke {...pokemon}/>
+      </div>
+      ):
+       searchTerm? (
+      <div className='flex items-center justify-center h-[85vh]'>
+        <h1 className='text-amber-950 text-center pixel'> Pokemon not found. </h1>
+      </div>
+      ):
+      (
+        <div className='flex items-center justify-center h-[85vh]'>
+        <h1 className='text-amber-950 text-center pixel'> Enter a pokemon's name. </h1>
+      </div>
+      )
+      }
+    </div>
+  )
+}
+
+export default App
---
+++ src/Poke.jsx
@@ -0,0 +1,84 @@
+    import { useState, useEffect, useRef } from 'react';
+    import './App.css';
+    import { capitalize } from './App.jsx';
+    import Rotate from './resources/rotate.svg';
+    const getBarColor = (value) => {
+        if (value<50) return 'bg-red-500';
+        if (value<90) return 'bg-yellow-500';
+        return 'bg-green-500';
+    }
+
+
+    const Pokemon = ( poke ) => {
+        if (!poke) return null;
+        const audioRef = useRef(null);
+        const [turned, setTurned] = useState(false);
+        useEffect(() => {
+        audioRef.current = new Audio(poke.cries.latest);
+        }, [poke]);
+
+        const playCry = () => {
+            audioRef.current.currentTime = 0;
+            audioRef.current.play();
+        }
+
+        const sprite = turned
+        ? poke.sprites.back_default
+        : poke.sprites.front_default;
+        return (
+            <div className="flex">
+                <div className="flex flex-col items-center">
+                    <img
+                    src={sprite}
+                    className='w-80 h-80 border-black border-6 m-4' 
+                    />
+                    <img src={Rotate} width="100" onClick={() => setTurned(prev => !prev)}></img>
+                </div>
+                <div className="flex flex-col bg-amber-50 p-2 rounded-lg">
+                    <div className='text-amber-950  pixel text-4xl m-4 flex flex-col'>
+                        <p> NAME - {capitalize(poke.name)} </p> 
+                    </div>
+                    <div className='text-gray-950 pixel text-2xl m-4 flex gap-30'>
+                        <p> Height - {poke.height} </p>
+                        <p> Weight - {poke.weight} </p>
+                    </div>
+                    <div className="flex gap-10 text-gray-950 pixel text-2xl m-4">
+                        <p className="text-amber-950"> TYPE: </p>
+                        {poke.types.map((t) => (
+                            <p key={t.slot}>
+                                {capitalize(t.type.name)}
+                            </p>
+                        ))}
+                    </div>
+                    <div className="flex justify-center items-center">
+                        <div className="w-md max-w-md bg-green-200">
+                            {poke.stats.map((stat) => (
+                                <div className="m-2 bg-green-100">
+                                    <div key={stat.stat.name} className="mb-4 text-amber-950 pixel ml-4">
+                                        <span> {stat.stat.name.toUpperCase()}</span>
+                                        <span> {stat.base_stat}</span>
+                                    </div>
+
+                                    <div className='w-full bg-gray-300 h-4 border border-black'>
+                                        <div className={`${getBarColor(stat.base_stat)} h-4`}
+                                            style={{ width: `${(stat.base_stat / 255)*100}% `}}
+                                        >
+                                        </div>
+                                    </div>
+                                </div> 
+                            ))}
+                        </div>
+                    <div className="text-black ml-80 text-2x1 pixel">
+                        <p> SOUND: </p>
+                    </div>
+                    <button
+                     className="h-20 text-white pixel"
+                     onClick={playCry}
+                     >CLICK </button>
+                </div>
+                </div>
+            </div>
+        )
+    }
+
+    export default Pokemon;
